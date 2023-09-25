c8925ea798b9c81c72d0d52210de07dd0e4d747b
Jian He <hejianpitt@gmail.com>
2023-09-25 00:04:01 -0400

    Added comments to the trimmed_mean function

--- R/trimmed_mean.R
+++ R/trimmed_mean.R
@@ -6,12 +6,15 @@
 
 trimmed_mean <- function(x, l, s) {
   if (any(!is.numeric(x), !is.numeric(c(l, s)))) {
+    # Check if the values provided by the users are numeric
     stop("Numeric values/vector are required!")
   } else if (length(x) < l + s + 1) {
+    # Check if the length of the numeric vector is greater than or equal to l + s + 1
     stop("The length of the numeric vector entered is too short!")
   } else {
-    max_l <- tail(x[order(x)], l)
-    min_s <- head(x[order(x)], s)
+    max_l <- tail(x[order(x)], l) # Select out the l largest values
+    min_s <- head(x[order(x)], s) # Select out the s smallest values
+    # Get the trimmed vector by filtering out the repetitive elements
     numbers_to_be_trimmed <- c(min_s, max_l)
     trimmed_x <- setdiff(x, numbers_to_be_trimmed)
     return(mean(trimmed_x))
